e0ca7c15cdcf67b54dbe0a8ddb1cc65289e9f951
Alexandra Zheleznova <alex20sent01love@gmail.com>
2026-04-09 20:17:16 +0300

    Create example_funcs.py, add func: - divide(a, b).

--- example_funcs.py
+++ example_funcs.py
@@ -9,9 +9,12 @@ def multiply(a, b):
 def subtract(a, b):
     return a - b
 
+def divide(a, b):
+    return a / b
     
 
 if __name__ == '__main__':
     print(add(3, 5))
     print(multiply(5, 3))
     print(subtract(5, 3))
+    print(divide(10, 2))
